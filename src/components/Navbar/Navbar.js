b2e83bf2587942d5b25f738dfa18ad28c08a2bad
Jose Campo Rivas <josecampor31@gmail.com>
2022-06-21 20:10:35 +0200

    add new row in Navbar
    add a new row in Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,15 +6,23 @@ import NavSearch from '../NavSearch/NavSearch'
 import Cart from '../Cart/Cart'
 
 export default function Navbar(){
+    const NAVBAR_BOTTOM_ITEMS = ["Best Sellers", "Latest News", "Offers", "Amazon Basics", "Books", "Beauty", "Electronics"]
+
     return(
         <div id='navbar'>
-            <img src={logoAmazon} alt='logo-amazon' id='logoAmazon' />
-            <Location />
-            <NavSearch />
-            {/* language picker, */}
-            <Cart />
-            {/* sidebar and different topics */}
+            <div id='navbar-top'>
+                <img src={logoAmazon} alt='logo-amazon' id='logoAmazon' />
+                <Location />
+                <NavSearch />
+                {/* language picker, */}
+                <Cart />
+            </div>
+            <div id='navbar-bottom'>
+                {/* sidebar */}
+                {NAVBAR_BOTTOM_ITEMS.map(elem => {
+                    return <a href={`#${elem}`} id={elem}>{elem}</a>
+                })}
+            </div>
         </div>
-
     )
 }
